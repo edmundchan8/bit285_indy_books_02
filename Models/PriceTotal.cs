ff73837990a40a9097958fc37e957fdd1035afcc
Gregory Jesionowski <gregory.a.jesionowski@gmail.com>
2020-02-27 16:29:32 -0800

    Class end

---
+++ Models/PriceTotal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndyBooks.Models
+{
+    public class PriceTotal
+    {
+        public decimal Price { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal TotalPrice { get; set; }
+
+
+    }
+}
